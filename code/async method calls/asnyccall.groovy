c03cb19e5e6b615ee3793a4741de6c1ccd9838e3
Jon DeJong <jon.dejong@gmail.com>
2014-07-26 19:33:45 -0500

    adding callAsync()

--- code/async method calls/asnyccall.groovy
+++ code/async method calls/asnyccall.groovy
@@ -1,3 +1,15 @@
-/**
- * Created by jondejong on 7/26/14.
- */
+import groovyx.gpars.GParsPool
+
+import java.util.concurrent.Future
+
+
+GParsPool.withPool {
+    Future longVal = {
+        (1..25).collect {
+            sleep 500
+            it * 2
+        }
+    }.callAsync()
+
+    println "val: ${longVal.get()}"
+}
